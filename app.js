9ecc24c14ea24a988245fb16267bf10de030e61a
wael <waelferjeni86@gmail.com>
2023-12-30 22:26:48 +0100

    first-commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const products = require('./data.js')
+const products_routes = require('./routes/products.js')
+
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI)
+    .then((result) => app.listen(5000))
+    .catch((err) => console.log(Error))
+
+app.use(express.json())
+app.use('/api/products', products_routes)
